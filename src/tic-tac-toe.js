2195dcda3172797e1b65b1b926ac9081b1f657ef
alexpodelinskii <nikitik007@yandex.ru>
2021-07-16 00:43:32 +0300

    refact: getWinner
    Signed-off-by: alexpodelinskii <nikitik007@yandex.ru>

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -3,62 +3,61 @@ class TicTacToe {
         this.currentPlayerSymbol = "x";
         this.field = [[null, null, null,], [null, null, null,], [null, null, null,]]
         this.turns = 0;
-        this.win = null;
-        this.winner = null;
-        this.flag = false;
-        this.ttt = 0;
+
+
     }
 
     getCurrentPlayerSymbol() {
         return this.currentPlayerSymbol;
     }
 
-    checkIsFinished(rowIndex, columnIndex) {
-        if (rowIndex == columnIndex) {
-            if (this.field[0][0] == this.field[1][1] && this.field[1][1] == this.field[2][2]) {
-                this.win = true;
-            }
-        } else if (this.field[rowIndex][0] == this.field[rowIndex][1] && this.field[rowIndex][1] == this.field[rowIndex][2]) {
-            this.win = true;
-        } else if (this.field[0][columnIndex] == this.field[1][columnIndex] && this.field[1][columnIndex] == this.field[2][columnIndex]) {
-            this.win = true;
-        } else if ((rowIndex == 0 && columnIndex == 2) || (rowIndex == 2 && columnIndex == 0)) {
-            if (this.field[2][0] == this.field[1][1] && this.field[1][1] == this.field[0][2]) {
-                this.win = true;
-            }
-        }
-        if (this.win) {
-            this.winner = this.currentPlayerSymbol;
-        }
-
-
-    }
-
 
 
 
     nextTurn(rowIndex, columnIndex) {
-        this.ttt++;
         if (!this.field[rowIndex][columnIndex]) {
             this.field[rowIndex][columnIndex] = this.currentPlayerSymbol;
             this.turns++;
-            if (this.turns > 4 || !this.win) {
-                this.checkIsFinished(rowIndex, columnIndex);
-            }
-            if (this.currentPlayerSymbol === "x") {
-                this.currentPlayerSymbol = "o";
-            } else { this.currentPlayerSymbol = "x"; }
+            this.currentPlayerSymbol = this.currentPlayerSymbol == "x" ? "o" : "x";
         }
     }
 
 
     isFinished() {
-        return this.win || this.turns == 9;
+        if (this.getWinner() || this.isDraw()) {
+            return true;
+        } else return false;
     }
 
     getWinner() {
-        return this.winner;
+        if (this.field[0][0] && (this.field[0][0] === this.field[0][1]) && (this.field[0][0] === this.field[0][2])) {
+            return this.field[0][0];
+        }
+        if (this.field[1][0] && (this.field[1][0] === this.field[1][1]) && (this.field[1][0] === this.field[1][2])) {
+            return this.field[1][0];
+        }
+        if (this.field[2][0] && (this.field[2][0] === this.field[2][1]) && (this.field[2][0] === this.field[2][2])) {
+            return this.field[2][0];
+        }
+        if (this.field[0][0] && (this.field[0][0] === this.field[1][0]) && (this.field[0][0] === this.field[2][0])) {
+            return this.field[0][0];
+        }
+        if (this.field[0][1] && (this.field[0][1] === this.field[1][1]) && (this.field[0][1] === this.field[2][1])) {
+            return this.field[0][1];
+        }
+        if (this.field[0][2] && (this.field[0][2] === this.field[1][2]) && (this.field[0][2] === this.field[2][2])) {
+            return this.field[0][2];
+        }
+        if (this.field[0][0] && (this.field[0][0] === this.field[1][1]) && (this.field[0][0] === this.field[2][2])) {
+            return this.field[0][0];
+        }
+        if (this.field[2][0] && (this.field[2][0] === this.field[1][1]) && (this.field[2][0] === this.field[0][2])) {
+            return this.field[2][0];
+        }
+        return null;
     }
+
+
 
     noMoreTurns() {
         if (this.turns == 9) {
@@ -69,11 +68,9 @@ class TicTacToe {
     }
 
     isDraw() {
-        if (this.turns == 9 && !this.win) {
-            return true;
-        } else {
-            return false;
-        }
+        if (this.noMoreTurns() && !this.getWinner()) {
+            return true
+        } else return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
